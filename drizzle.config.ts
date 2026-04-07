e72e59884a3f43d193ccd76d3f0d6acdad7922fa
jamisonpence <jamisonpence@gmail.com>
2026-04-07 13:27:38 -0500

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  out: "./migrations",
-  schema: "./shared/schema.ts",
-  dialect: "sqlite",
-  dbCredentials: {
-    url: "./data.db",
-  },
+    out: "./migrations",
+    schema: "./shared/schema.ts",
+    dialect: "postgresql",
+    dbCredentials: {
+          url: process.env.DATABASE_URL!,
+    },
 });
